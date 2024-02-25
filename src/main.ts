8a2ea606dcbf6bff757e18406fe6b4afbc5f1f28
Hafidil Asad <hafidilaksad@gmail.com>
2024-02-25 14:15:08 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 
-  await app.listen(3500);
+  await app.listen(3500, '0.0.0.0');
 }
 bootstrap();
